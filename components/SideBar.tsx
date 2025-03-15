3a461e084cc333a9614b93b07c09fd9707a76cbc
DevChawla <119198647+DevChawla@users.noreply.github.com>
2025-03-15 21:41:43 +0530

    auth page

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import { sidebarLinks } from "@/constants";
 import { cn } from "@/lib/utils";
 import { usePathname } from "next/navigation";
+import Footer from "./Footer";
 
 const SideBar = ({ user }: SiderbarProps) => {
   const pathName = usePathname();
@@ -49,7 +50,7 @@ const SideBar = ({ user }: SiderbarProps) => {
         USER
       </nav>
 
-      Footer
+      <Footer user={user} type="desktop" />
     </section>
   );
 };
