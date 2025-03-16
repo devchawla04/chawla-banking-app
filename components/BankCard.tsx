9708be52a86e3460430722e0c1445617707a9e96
DevChawla <119198647+DevChawla@users.noreply.github.com>
2025-03-16 17:44:05 +0530

    transfer funds

--- components/BankCard.tsx
+++ components/BankCard.tsx
@@ -1,52 +1,63 @@
-import { formatAmount } from '@/lib/utils'
-import Link from 'next/link'
-import React from 'react'
-import Image from 'next/image'
+import { formatAmount } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import Copy from "./Copy";
 
-const BankCard = ({account,userName,showBalance}: CreditCardProps) => {
+const BankCard = ({
+  account,
+  userName,
+  showBalance = true,
+}: CreditCardProps) => {
+  console.log(account);
   return (
-    <div className='flex flex-col'>
-    <Link href="/" className='bank-card'>
-    <div className="bank-card_content">
-        <div>
-            <h1 className='text-16 font-semibold text-white'>
-                {userName}
-            </h1>
-            <p className="font-ibm-plex-serif font-black text-white">{formatAmount(account.currentBalance)}</p>
-        </div>
-        <article className="flex-col flex gap-2">
-            <div className="flex justify-between">
-                <h1 className='text-12 font-semibold text-white'>{userName}</h1>
-                <h2 className='text-12 font-semibold text-white'>●● / ●●</h2>
-            </div>
-            <p className="text-14 font-semibold tracking-[1.1] text-white">
-            ●●●● ●●●● ●●●● <span className="text-16">{account?.mask}</span>
+    <div className="flex flex-col">
+      <Link
+        href={`/transaction-history/?id=${account.appwriteItemId}`}
+        className="bank-card"
+      >
+        <div className="bank-card_content">
+          <div>
+            <h1 className="text-16 font-semibold text-white">{account.name}</h1>
+            <p className="font-ibm-plex-serif font-black text-white">
+              {formatAmount(account.currentBalance)}
             </p>
+          </div>
 
-        </article>
-    </div>
-    <div className="bank-card_icon">
-        <Image src="/icons/Paypass.svg"
-        width={20}
-        height={24}
-        alt='pay'
-        />
-        <Image src="/icons/mastercard.svg"
-        width={45}
-        height={32}
-        alt='mastercard'
-        className='ml-5'
-        />
-    </div>
-    <Image src="/icons/lines.png"
-        width={316}
-        height={190}
-        alt='lines'
-        className='absolute top-0 left-0'
-        />
-    </Link>
-    </div>
-  )
-}
+          <article className="flex flex-col gap-2">
+            <div className="flex justify-between">
+              <h1 className="text-12 font-semibold text-white">{userName}</h1>
+              <h2 className="text-12 font-semibold text-white">●● / ●●</h2>
+            </div>
+            <p className="text-14 font-semibold tracking-[1.1px] text-white">
+              ●●●● ●●●● ●●●● <span className="text-16">{account?.mask}</span>
+            </p>
+          </article>
+        </div>
 
-export default BankCard
+        <div className="bank-card_icon">
+          <Image src="/icons/Paypass.svg" width={20} height={24} alt="pay" />
+          <Image
+            src="/icons/mastercard.svg"
+            width={45}
+            height={32}
+            alt="mastercard"
+            className="ml-5"
+          />
+        </div>
+
+        <Image
+          src="/icons/lines.png"
+          width={316}
+          height={190}
+          alt="lines"
+          className="absolute top-0 left-0"
+        />
+      </Link>
+
+      {showBalance && <Copy title={account?.sharableId} />}
+    </div>
+  );
+};
+
+export default BankCard;
